4941ae5f71e855a31af51b7ee0ca268fd5e6b76f
Rajnoor-Brar <rajnoorsbrar@outlook.in>
2025-03-01 08:03:31 +0530

    Initialized: 	new file:   Home.html 	new file:   css/style.css 	new file:   js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("RAJNOOR home page loaded.");
+    
+    // Add any additional interactivity here.
+  });
+  
