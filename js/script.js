7e8c6f79987d6d97c4e99eaa05ef7e6364680350
Rajnoor-Brar <rajnoorsbrar@outlook.in>
2025-03-17 23:30:58 +0530

    Better Darked

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
+    
+    document.querySelectorAll("button").forEach(button => {
+        button.addEventListener("click", function() {
+            this.blur(); // Removes focus after click
+        });
+    });
+    
     const themeToggleBtn = document.getElementById("themeToggle");
     const themeIcon = document.getElementById("themeIcon");
   
@@ -9,13 +16,20 @@ document.addEventListener("DOMContentLoaded", function () {
       // After the animation duration (0.3s), toggle the theme and update the icon
       setTimeout(() => {
         let currentTheme = document.documentElement.getAttribute("data-bs-theme");
-  
+        let metaThemeColor = document.querySelector("meta[name=theme-color]");
+        let black = getComputedStyle(document.documentElement).getPropertyValue('--my-black').trim();
+
         if (currentTheme === "dark") {
             document.documentElement.setAttribute("data-bs-theme", "light");
             themeIcon.classList.remove("bi-moon-fill");
             themeIcon.classList.add("bi-sun-fill");
             themeToggleBtn.classList.remove("btn-outline-light");
             themeToggleBtn.classList.add("btn-outline-dark");
+            metaThemeColor.setAttribute("content", black);
+
+            document.querySelectorAll(".signatures").forEach(el => {
+                el.classList.remove("invert");
+            });
         } 
         else {
             document.documentElement.setAttribute("data-bs-theme", "dark");
@@ -23,6 +37,11 @@ document.addEventListener("DOMContentLoaded", function () {
             themeIcon.classList.add("bi-moon-fill");
             themeToggleBtn.classList.remove("btn-outline-dark");
             themeToggleBtn.classList.add("btn-outline-light");
+            metaThemeColor.setAttribute("content", black);
+
+            document.querySelectorAll(".signatures").forEach(el => {
+                el.classList.add("invert");
+            });
         }
         // Remove the rotation class so it can animate again next time
         themeIcon.classList.remove("rotate");
